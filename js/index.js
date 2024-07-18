b72e126f2daf54561714560fc0da01ef5f7ff758
wdjok <118990993+wdjok@users.noreply.github.com>
2024-07-18 14:58:36 +0800

    new

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 var day=document.getElementById('day');
+var paper=document.getElementById('paper');
 var date=new Date("2023-02-08");
 var today=new Date()
+
+function ShowPaper()
+{
+    paper.style.visibility="visible"
+}
 
 var diffTime=Math.abs(date-today)
 var diffDays=Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
-// day.innerText=diffDays;
-day.innerText='521';
+day.innerText=diffDays;
