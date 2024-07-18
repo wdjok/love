87a1dd212ff01773397ac2a7d5436d1f1733aa5e
wdjok <118990993+wdjok@users.noreply.github.com>
2024-07-18 22:32:15 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,4 +11,5 @@ function ShowPaper()
 var diffTime=Math.abs(date-today)
 var diffDays=Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
-day.innerText=diffDays;
+// day.innerText=diffDays;
+day.innerText='521';
