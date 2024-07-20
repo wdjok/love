bcff9b62c8fb4cd2a7a56fd361a0e50f92586c0f
wdjok <118990993+wdjok@users.noreply.github.com>
2024-07-21 03:12:41 +0800

    new

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-var day=document.getElementById('day');
-var paper=document.getElementById('paper');
-var date=new Date("2023-02-08");
-var today=new Date()
-
-function ShowPaper()
+window.onload=function()
 {
+    var day=document.getElementById('day');
+    var date=new Date("2023-02-08");
+    var today=new Date()
+    var diffTime=Math.abs(date-today)
+    var diffDays=Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    day.innerHTML=diffDays;
+}
+function ShowPaper()
+{   
+    var paper=document.getElementById('paper');
     paper.style.visibility="visible"
 }
-
-var diffTime=Math.abs(date-today)
-var diffDays=Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-
-day.innerText=diffDays;
