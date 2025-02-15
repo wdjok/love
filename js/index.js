de931b81a0140ecf4ade31730ded7de53be3c55d
wdjok <118990993+wdjok@users.noreply.github.com>
2025-02-16 01:39:20 +0800

    new

--- js/index.js
+++ js/index.js
@@ -10,8 +10,7 @@ function getday()
     const date = new Date(2023, 1, 8) // 月份从 0 开始：1 表示 2 月
     const today = new Date()
     const diffTime = today - date
-    // day.innerHTML = Math.floor(diffTime / (1000 * 60 * 60 * 24))
-    day.innerHTML = 731
+    day.innerHTML = Math.floor(diffTime / (1000 * 60 * 60 * 24))
 }
 
 function show()
